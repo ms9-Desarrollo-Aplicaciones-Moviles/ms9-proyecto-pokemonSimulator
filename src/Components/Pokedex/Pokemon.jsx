e6d80d3107eb650be54ed4681badec1d8e30d048
Miguel Alfonso Córdoba Rojas <18470271@campeche.tecnm.mx>
2022-12-31 19:42:30 -0600

    Fix: Estilos agregados al botón de volver.

--- src/Components/Pokedex/Pokemon.jsx
+++ src/Components/Pokedex/Pokemon.jsx
@@ -6,6 +6,10 @@ const Pokemon = () => {
 	const location = useLocation()
 	let pokemon = location.state
 
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1)
+	}
+
 	function renderSwitchTiposPokemon(param) {
 		switch (param.type.name) {
 			case 'normal':
@@ -179,90 +183,77 @@ const Pokemon = () => {
 
 	return (
 		<>
-		<div className='header-main-pokemon'>
-						<span className='number-pokemon'>#{pokemon.id}</span>
-						<div className='container-img-pokemon'>
-							<img
-								src={pokemon.sprites.other.dream_world.front_default}
-								alt={`Pokemon ${pokemon.name}`}
-							/>
-						</div>
+			<div className="header-main-pokemon">
+				<span className="number-pokemon">#{pokemon.id}</span>
+				<div className="container-img-pokemon">
+					<img
+						src={pokemon.sprites.other.dream_world.front_default}
+						alt={`Pokemon ${pokemon.name}`}
+					/>
+				</div>
 
-						<div className='container-info-pokemon'>
-							<h1>{pokemon.name}</h1>
-							<div className='card-types info-pokemon-type'>
-							{pokemon.types.map((tipoPokemon) => {
-									return renderSwitchTiposPokemon(tipoPokemon)
-								})}
-							</div>
-							<div className='info-pokemon'>
-								<div className='group-info'>
-									<p>Altura</p>
-									<span>{pokemon.height}</span>
-								</div>
-								<div className='group-info'>
-									<p>Peso</p>
-									<span>{pokemon.weight}KG</span>
-								</div>
-							</div>
+				<div className="container-info-pokemon">
+					<h1>{capitalizeFirstLetter(pokemon.name)}</h1>
+					<div className="pokemon-types">
+						{pokemon.types.map((tipoPokemon) => {
+							return renderSwitchTiposPokemon(tipoPokemon)
+						})}
+					</div>
+					<div className="info-pokemon">
+						<div className="group-info">
+							<p className="m-0">Altura</p>
+							<span>{pokemon.height / 10} Metros</span>
+						</div>
+						<div className="group-info">
+							<p className="m-0">Peso</p>
+							<span>{pokemon.weight / 10} Kilogramos</span>
 						</div>
 					</div>
+				</div>
+			</div>
 
-
-
-					<div className='container-stats'>
-						<h1>Estadísticas</h1>
-						<div className='stats'>
-							<div className='stat-group'>
-								<span>Hp</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[0].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Ataque</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[1].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Defensa</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[2].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Ataque Especial</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[3].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Defensa Especial</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[4].base_stat}
-								</span>
-							</div>
-							<div className='stat-group'>
-								<span>Velocidad</span>
-								<div className='progress-bar'></div>
-								<span className='counter-stat'>
-									{pokemon.stats[5].base_stat}
-								</span>
-							</div>
-						</div>
+			<div className="container-stats">
+				<h1>Estadísticas</h1>
+				<div className="stats">
+					<div className="stat-group">
+						<span>Hp</span>
+						<div className="progress-bar"></div>
+						<span className="counter-stat">{pokemon.stats[0].base_stat}</span>
 					</div>
+					<div className="stat-group">
+						<span>Ataque</span>
+						<div className="progress-bar"></div>
+						<span className="counter-stat">{pokemon.stats[1].base_stat}</span>
+					</div>
+					<div className="stat-group">
+						<span>Defensa</span>
+						<div className="progress-bar"></div>
+						<span className="counter-stat">{pokemon.stats[2].base_stat}</span>
+					</div>
+					<div className="stat-group">
+						<span>Ataque Especial</span>
+						<div className="progress-bar"></div>
+						<span className="counter-stat">{pokemon.stats[3].base_stat}</span>
+					</div>
+					<div className="stat-group">
+						<span>Defensa Especial</span>
+						<div className="progress-bar"></div>
+						<span className="counter-stat">{pokemon.stats[4].base_stat}</span>
+					</div>
+					<div className="stat-group">
+						<span>Velocidad</span>
+						<div className="progress-bar"></div>
+						<span className="counter-stat">{pokemon.stats[5].base_stat}</span>
+					</div>
+				</div>
+			</div>
 
-
-					<div class="pokebola">
-		<div class="pokebola-botao" type="button"
-						onClick={() => path('/pokedex')}></div></div>
-
+			<div className="contenedor-pokebola">
+				<h2>Volver ➡ </h2>
+				<div className="pokebola" type="button" onClick={() => path('/pokedex')}>
+					<div className="pokebola-botao"></div>
+				</div>
+			</div>
 		</>
 	)
 }
