132cd495d40b6fc3ce79a1366f721493c57f72d1
Diana Sabala Pérez <diana.sabala@hotmail.com>
2023-01-01 17:40:04 -0600

    Animación pikachu salto clic y animación bokeball.

--- src/Components/Inicio/ScreenInicio.jsx
+++ src/Components/Inicio/ScreenInicio.jsx
@@ -1,4 +1,5 @@
-import { render } from 'react-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ScreenInicio.css';
 
@@ -39,10 +40,25 @@ const ScreenInicio = () => {
             name: 'Galar'
         },
     ];
+
+    const [clic, setClic] = useState(0);
+    function Jump() {
+        const pikachu = document.getElementById('player-pikachu');
+        pikachu.classList.add('pikachuJump');
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const pikachu = document.getElementById('player-pikachu');
+            pikachu.classList.remove('pikachuJump');
+          }, 600);
+
+          return () => clearInterval(interval);
+      }, [clic]);
     
     return (
         <>
-        <div id='board' className="container-inicio">
+        <div className="container-inicio">
             <div className='background-image'> </div>
             <div className='logo'>
                 <img src='./img/logo.png'></img>
@@ -50,19 +66,25 @@ const ScreenInicio = () => {
             <div className='dropDownRegion'>
                 <h1 className='headerRegion'>Selecciona la región: </h1>
                 <select className='selectRegion'>
-                    {regiones.map(region => <option 
-                    key={region.value} 
+                    {regiones.map(region => <option
+                    key={region.value}
                     value={region.value}>{region.name}</option>)}
                 </select>
             </div>
+            <div id='player-pikachu' className='pikachu' onClick={() => {Jump(); setClic(clic + 1)}}>
+                <img className='pikachu-img' src='./img/pikachu-running.gif'></img>
+                {/* https://media.tenor.com/fSsxftCb8w0AAAAi/pikachu-running.gif */}
+            </div>
+            <div id='pokeball' className='pokeball'>
+                <img className='pokeball-img' src='./img/pokeball-inicio.gif'></img>
+            </div>
+            {/* <div className='score'>
+                Esquivadas: <span id='score'>0</span>
+            </div> */}
             <div className='button-aceptar'>
                 <button className='btn-aceptar' onClick={() => {path('/pokedex')}}>
                     Aceptar
-                </button>                
-            </div>
-            <div id='player-pikachu' className='pikachu'>
-                <img className='pikachu-img' src='./img/pikachu-running.gif'></img>
-                {/* https://media.tenor.com/fSsxftCb8w0AAAAi/pikachu-running.gif */}
+                </button>
             </div>
         </div>
         </>
